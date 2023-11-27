b9aa0a6ecfc67225668304454cdad75ff983ceb3
mohadesarizwanii <zain.raza@viabletree.com>
2023-11-27 23:22:57 +0500

    ui updated in get money section(2)

--- resources/js/Pages/HomePage/HeroSectionTwo.vue
+++ resources/js/Pages/HomePage/HeroSectionTwo.vue
@@ -5,19 +5,17 @@ import GetMoneySection from "@/Pages/HomePage/partials/GetMoneySection.vue";
 
 <template>
     <div class="hero-section-two-container">
-        <FiftyText
-            class="limited-title-width"
-            color="dark"
-            variation="heading-2"
-        >
-            Peer Swap to US/Canada from Nigeria & vice versa
-        </FiftyText>
+        <div class="inner-wrap-two">
+            <FiftyText class="limited-title-width" color="dark" variation="heading-2">
+                Peer Swap to US/Canada from Nigeria & vice versa
+            </FiftyText>
 
-        <FiftyText class="mt-10 limited-title-text">
-            Instantly! hassle-free! and with little to no fees! Use our website
-            or app to connect with a peer and instantly exchange money between
-            US & Nigeria using direct bank deposits!
-        </FiftyText>
+            <FiftyText class="mt-10 limited-title-text">
+                Instantly! hassle-free! and with little to no fees! Use our website
+                or app to connect with a peer and instantly exchange money between
+                US & Nigeria using direct bank deposits!
+            </FiftyText>
+        </div>
 
         <GetMoneySection />
     </div>
