9444dca34b14f45bd2b795c8a34347872fb73cab
mohadesarizwanii <zain.raza@viabletree.com>
2023-11-27 23:06:13 +0500

    ui updated in get money section

--- resources/js/Pages/HomePage/partials/GetMoneySection.vue
+++ resources/js/Pages/HomePage/partials/GetMoneySection.vue
@@ -4,29 +4,29 @@ import NewActionButton from "@/Components/Design/NewActionButton.vue";
 </script>
 
 <template>
-    <div class="get-money-section-container" >
-        
+    <div class="get-money-section-cont">
+
         <div class="content-container">
-            <div>
+            <div class="inner-container">
 
                 <FiftyText color="dark" variation="heading-3">
                     Peer to peer money exchange the way it was meant to be!
                 </FiftyText>
-    
-                <FiftyText class="body-text">
-                    Our dream is for people to live and work anywhere seamlessly.
-                    That means money without borders: moving it instantly,
-                    transparently, conveniently, and eventually for free.
-                </FiftyText>
+
+            </div>
+            <div class="inner-cont-bg">
+                <div class="inner-cont-info">
+                    <FiftyText class="body-text">
+                        Our dream is for people to live and work anywhere seamlessly.
+                        That means money without borders: moving it instantly,
+                        transparently, conveniently, and eventually for free.
+                    </FiftyText>
+
+                    <NewActionButton :url="route('user.info.page')" class="w-fit money-action-button"
+                        title="Start currency swap" />
+                </div>
             </div>
 
-            <NewActionButton
-                :url="route('user.info.page')"
-                class="w-fit money-action-button"
-                title="Start currency swap"
-
-                />
-                <!-- title="Start Posting" -->
         </div>
     </div>
 </template>
